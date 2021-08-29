69b3b0c34e54052ebd9d0ba300bc8222fa5ec5f5
acc username <acc email>
2021-08-29 19:34:31 +0600

    ali

--- api.js
+++ api.js
@@ -6,17 +6,34 @@ const mobile =()=>{
 mobile();
 const bababhandari=data=>{
     //  console.log(data);
-     for(const element of data){
-         
-        document.getElementById('buttonadd').addEventListener('click',function(){
-            const uldata=document.getElementById('uldata');
-         const li =document.createElement('li');
-         li.innerText=`email: ${element.email}
-                name:${element.name} `;
-         const div =document.getElementById("cumma");
-         div.appendChild(li);
-        })
-         
+     
+    data.forEach(element=>
+        
+        {console.log(element.name);
+            document.getElementById("buttonadd").addEventListener('click',function(){
+              const uldata =document.getElementById('uldata');
+            const li = document.createElement('li');
+            li.innerText=`${element.email} ${element.name}`;
+            const div =document.getElementById('cumma');
+            div.appendChild(li);  
+            })
+            
+        
+        
+        
+        });
+    
+   
+
+        // document.getElementById('buttonadd').addEventListener('click',function(){
+        //     const uldata=document.getElementById('uldata');
+        //  const li =document.createElement('li');
+        //  li.innerText=`email: ${element.email}
+        //         name:${element.name} `;
+        //  const div =document.getElementById("cumma");
+        //  div.appendChild(li);
+        
+          
      }
-}
+
 
